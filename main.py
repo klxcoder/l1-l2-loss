1a263b088f43451dc999ef80e21712d717b686d4
klxcoder <klxcoder@gmail.com>
2025-04-05 17:22:35 +0700

    plot L1 loss and L2 loss

--- main.py
+++ main.py
@@ -30,41 +30,52 @@ def get_derivatives(
         loss_fn: Callable[[NDArray[np.float64], NDArray[np.float64]], float],
     ):
     small = 0.01
-    dloss_dw = (loss_fn(y, (w+small)*x + b) - loss_fn(y, w*x + b)) / small
-    dloss_db = (loss_fn(y, w*x + (b+small)) - loss_fn(y, w*x + b)) / small
-    return dloss_dw, dloss_db
+    loss = loss_fn(y, w*x + b)
+    dloss_dw = (loss_fn(y, (w+small)*x + b) - loss) / small
+    dloss_db = (loss_fn(y, w*x + (b+small)) - loss) / small
+    return dloss_dw, dloss_db, loss
 
 def get_best_fit_line(
         x: NDArray[np.float64],
         y: NDArray[np.float64],
         loss_fn: Callable[[NDArray[np.float64], NDArray[np.float64]], float],
     ):
-    w = 0
-    b = 0
-    times = 1000
-    alpha = 0.01
+    w: float = 0
+    b: float = 0
+    times: int = 1000
+    alpha: float = 0.01
+    x_loss: list[float] = []
+    y_loss: list[float] = []
     for _ in range(times):
-        dloss_dw, dloss_db = get_derivatives(x, y, w, b, loss_fn)
+        dloss_dw, dloss_db, loss = get_derivatives(x, y, w, b, loss_fn)
+        x_loss.append(len(x_loss))
+        y_loss.append(loss)
         w -= alpha * dloss_dw
         b -= alpha * dloss_db
-    return w, b
+    return w, b, x_loss, y_loss
 
 def main():
     x, y = get_data()
 
-    w1, b1 = get_best_fit_line(x, y, get_l1_loss)
+    w1, b1, x1_loss, y1_loss = get_best_fit_line(x, y, get_l1_loss)
     y1_pred = w1*x + b1
 
-    w2, b2 = get_best_fit_line(x, y, get_l2_loss)
+    w2, b2, x2_loss, y2_loss = get_best_fit_line(x, y, get_l2_loss)
     y2_pred = w2*x + b2
 
     # Create subplots
     _, axes = plt.subplots(2, 2, figsize=(12, 10))
+
     axes[0, 0].scatter(x, y, color='blue', label='Data points')
     axes[0, 0].plot(x, y1_pred, color='red', label='L1 predicted line')
     axes[0, 0].plot(x, y2_pred, color='green', label='L2 predicted line')
     axes[0, 0].set_title('Data and predicted lines')
     axes[0, 0].legend()
+
+    axes[0, 1].plot(x1_loss, y1_loss, color='red', label='L1 loss')
+    axes[0, 1].plot(x2_loss, y2_loss, color='green', label='L2 loss')
+    axes[0, 1].set_title('Losses')
+    axes[0, 1].legend()
 
     plt.show()
 
