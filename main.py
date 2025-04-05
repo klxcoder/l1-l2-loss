db1eae21e73e389feb2115421e193f1453f463a7
klxcoder <klxcoder@gmail.com>
2025-04-05 16:18:41 +0700

    plot predicted lines

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import numpy as np
 
 def get_data():
     np.random.seed(0)
-    x = np.linspace(0, 10, 100).reshape(-1, 1)
-    y = 2 * x.squeeze() + np.random.normal(0, 1, x.shape[0])
-    y[30] = 30  # Add an outlier
-    return x, y
+    x = np.arange(10)
+    y = 2 * x + np.random.normal(0, 1, x.shape[0])
+    y_pred_l1 = 2*x + 0.5
+    y_pred_l2 = 2*x + 1
+    y[2] = 100  # Add an outlier
+    return x, y, y_pred_l1, y_pred_l2
 
 def main():
-    x, y = get_data()
-    plt.scatter(x, y)
+    x, y, y_pred_l1, y_pred_l2 = get_data()
+
+    # Create subplots
+    _, axes = plt.subplots(2, 2, figsize=(12, 10))
+    axes[0, 0].scatter(x, y, color='blue', label='Data points')
+    axes[0, 0].plot(x, y_pred_l1, color='red', label='L1 predicted line')
+    axes[0, 0].plot(x, y_pred_l2, color='green', label='L2 predicted line')
+    axes[0, 0].set_title('Data and predicted lines')
+    axes[0, 0].legend()
+
     plt.show()
 
 if __name__ == "__main__":
