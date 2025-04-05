3d302817a2887c7e4a9c3818f0b9bac0d7a0b497
klxcoder <klxcoder@gmail.com>
2025-04-05 16:08:48 +0700

    get_data with 1 outliner

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def get_data():
+    np.random.seed(0)
+    x = np.linspace(0, 10, 100).reshape(-1, 1)
+    y = 2 * x.squeeze() + np.random.normal(0, 1, x.shape[0])
+    y[30] = 30  # Add an outlier
+    return x, y
 
 def main():
-    plt.plot([1, 2, 3])
+    x, y = get_data()
+    plt.scatter(x, y)
     plt.show()
 
 if __name__ == "__main__":
